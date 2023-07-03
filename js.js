beddae23a745e1c07ae320ca034f725aa055e08f
User <132357387+N0CodeN0Life@users.noreply.github.com>
2023-07-04 00:41:21 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+function loadNextImage() {
+    var widthInput = document.getElementById('width-input');
+    var heightInput = document.getElementById('height-input');
+    var image = document.getElementById('image');
+    var loadingIndicator = document.getElementById('loading-indicator');
+
+    var width = parseInt(widthInput.value);
+    var height = parseInt(heightInput.value);
+
+    image.style.display = 'none';
+    loadingIndicator.style.display = 'block';
+
+    var img = new Image();
+    img.onload = function() {
+        image.src = img.src;
+        image.style.display = 'block';
+        loadingIndicator.style.display = 'none';
+    };
+    img.src = 'https://unsplash.it/' + width + '/' + height + '?random=' + new Date().getTime();
+}
